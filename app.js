017b2937753e3e839357de9530f6419e7bdfb832
Hamish Willee <hamishwillee@gmail.com>
2016-12-19 16:21:31 +1100

    Add gzip compression to responses

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var expressValidator = require('express-validator');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var catalog = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
 
 // Create the Express application object
 var app = express();
@@ -32,6 +33,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator() ); // Add this after the bodyParser middleware!
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
