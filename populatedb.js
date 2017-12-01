565a1d3ee6c92b52276a8fbabb69feb60b376277
Hamish Willee <hamishwillee@gmail.com>
2017-12-01 14:44:20 +1100

    Update populatedb to use native promise library and new connection option

--- populatedb.js
+++ populatedb.js
@@ -18,7 +18,10 @@ var BookInstance = require('./models/bookinstance')
 
 var mongoose = require('mongoose');
 var mongoDB = userArgs[0];
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
