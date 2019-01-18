9708150f55aff298b2af68c762173ba29a2cd475
icehongssi <icehongssii@gmail.com>
2019-01-18 12:41:13 +0900

    Update populatedb.js
    Delete unused variable

--- populatedb.js
+++ populatedb.js
@@ -20,7 +20,6 @@ var mongoose = require('mongoose');
 var mongoDB = userArgs[0];
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
-var db = mongoose.connection;
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var authors = []
