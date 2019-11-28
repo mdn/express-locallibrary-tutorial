5e894d56ee01ac34188229ea369255a6933bd7ba
benneee <benedictiknkeonye@gmail.com>
2019-11-29 00:06:55 +0100

    Checked for names before returning virtual

--- models/author.js
+++ models/author.js
@@ -3,52 +3,49 @@ var moment = require('moment'); // For date handling.
 
 var Schema = mongoose.Schema;
 
-var AuthorSchema = new Schema(
-    {
-    first_name: {type: String, required: true, max: 100},
-    family_name: {type: String, required: true, max: 100},
-    date_of_birth: { type: Date },
-    date_of_death: { type: Date },
-    }
-  );
+var AuthorSchema = new Schema({
+  first_name: { type: String, required: true, max: 100 },
+  family_name: { type: String, required: true, max: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date }
+});
 
 // Virtual for author "full" name.
-AuthorSchema
-.virtual('name')
-.get(function () {
-  return this.family_name +', '+this.first_name;
+AuthorSchema.virtual('name').get(function() {
+  var fullname = '';
+
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name;
+  }
+
+  if (!this.first_name && !this.family_name) {
+    fullname = '';
+  }
+  return fullname;
 });
 
 // Virtual for this author instance URL.
-AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/'+this._id
+AuthorSchema.virtual('url').get(function() {
+  return '/catalog/author/' + this._id;
 });
 
-AuthorSchema
-.virtual('lifespan')
-.get(function () {
-  var lifetime_string='';
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifetime_string = '';
   if (this.date_of_birth) {
-      lifetime_string=moment(this.date_of_birth).format('MMMM Do, YYYY');
-      }
-  lifetime_string+=' - ';
+    lifetime_string = moment(this.date_of_birth).format('MMMM Do, YYYY');
+  }
+  lifetime_string += ' - ';
   if (this.date_of_death) {
-      lifetime_string+=moment(this.date_of_death).format('MMMM Do, YYYY');
-      }
-  return lifetime_string
+    lifetime_string += moment(this.date_of_death).format('MMMM Do, YYYY');
+  }
+  return lifetime_string;
 });
 
-AuthorSchema
-.virtual('date_of_birth_yyyy_mm_dd')
-.get(function () {
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
   return moment(this.date_of_birth).format('YYYY-MM-DD');
 });
 
-AuthorSchema
-.virtual('date_of_death_yyyy_mm_dd')
-.get(function () {
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
   return moment(this.date_of_death).format('YYYY-MM-DD');
 });
 
