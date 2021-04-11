bf5ad6f752fc4eccd1d9c26a64f2e89259f14b81
Hamish Willee <hamishwillee@gmail.com>
2021-04-12 09:20:21 +1000

    genre: fix up maxLength, minLength

--- models/genre.js
+++ models/genre.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema({
-    name: {type: String, required: true, minlength: 3, maxlength: 100}
+    name: {type: String, required: true, minLength: 3, maxLength: 100}
 });
 
 // Virtual for this genre instance URL.
