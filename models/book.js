7b999f6375ac61a74eb6883045f8bb06920eb47e
kmoser <kmoser@users.noreply.github.com>
2021-11-14 14:54:33 -0500

    Update book.js
    Changed ref value for 'genre' in BookSchema to properly reference the GenreInstance collection

--- models/book.js
+++ models/book.js
@@ -7,7 +7,7 @@ var BookSchema = new Schema({
     author: { type: Schema.ObjectId, ref: 'Author', required: true },
     summary: {type: String, required: true},
     isbn: {type: String, required: true},
-    genre: [{ type: Schema.ObjectId, ref: 'Genre' }]
+    genre: [{ type: Schema.ObjectId, ref: 'GenreInstance' }]
 });
 
 // Virtual for this book instance URL.
