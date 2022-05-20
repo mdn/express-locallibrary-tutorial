b986e9c84b388f2d4847074a882637bfaf37cfef
Eero Pomell <97628152+kyopa@users.noreply.github.com>
2022-05-20 22:24:52 +0300

    Update var with const and let

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,6 @@
-var Author = require('../models/author')
-var async = require('async')
-var Book = require('../models/book')
+const Author = require('../models/author')
+const async = require('async')
+const Book = require('../models/book')
 
 const { body,validationResult } = require("express-validator");
 
@@ -32,7 +32,7 @@ exports.author_detail = function (req, res, next) {
     }, function (err, results) {
         if (err) { return next(err); } // Error in API usage.
         if (results.author == null) { // No results.
-            var err = new Error('Author not found');
+            let err = new Error('Author not found');
             err.status = 404;
             return next(err);
         }
@@ -65,7 +65,7 @@ exports.author_create_post = [
         const errors = validationResult(req);
         
         // Create Author object with escaped and trimmed data
-        var author = new Author(
+        ' author = new Author(
             {
                 first_name: req.body.first_name,
                 family_name: req.body.family_name,
@@ -152,7 +152,7 @@ exports.author_update_get = function (req, res, next) {
     Author.findById(req.params.id, function (err, author) {
         if (err) { return next(err); }
         if (author == null) { // No results.
-            var err = new Error('Author not found');
+            let err = new Error('Author not found');
             err.status = 404;
             return next(err);
         }
@@ -181,7 +181,7 @@ exports.author_update_post = [
         const errors = validationResult(req);
 
         // Create Author object with escaped and trimmed data (and the old id!)
-        var author = new Author(
+        const author = new Author(
             {
                 first_name: req.body.first_name,
                 family_name: req.body.family_name,
