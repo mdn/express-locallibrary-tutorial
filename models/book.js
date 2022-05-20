a5cce7b86dc2fc81ca2936328a53e6918ee18f43
Eero Pomell <97628152+kyopa@users.noreply.github.com>
2022-05-20 22:28:57 +0300

    Replace var with let and const

--- models/book.js
+++ models/book.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-var BookSchema = new Schema({
+const BookSchema = new Schema({
     title: {type: String, required: true},
     author: { type: Schema.ObjectId, ref: 'Author', required: true },
     summary: {type: String, required: true},
