261ad23c2c8188cca5aeeaf5382220ca74444dba
Hamish Willee <hamishwillee@gmail.com>
2023-01-06 12:23:48 +1100

    Revert stuff from generator

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 const compression = require('compression');
 const helmet = require('helmet');
 
-const app = express();
+var app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
