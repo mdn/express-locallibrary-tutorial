82b14ab729d82bb84b875ccaa7b75683f1b04f1e
Hamish Willee <hamishwillee@gmail.com>
2023-01-06 11:51:36 +1100

    populatedb - use const not var

--- populatedb.js
+++ populatedb.js
@@ -3,40 +3,36 @@
 console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"');
 
 // Get arguments passed on command line
-var userArgs = process.argv.slice(2);
-/*
-if (!userArgs[0].startsWith('mongodb')) {
-    console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-    return
-}
-*/
-var async = require('async')
-var Book = require('./models/book')
-var Author = require('./models/author')
-var Genre = require('./models/genre')
-var BookInstance = require('./models/bookinstance')
+const userArgs = process.argv.slice(2);
+
+const async = require('async')
+const Book = require('./models/book')
+const Author = require('./models/author')
+const Genre = require('./models/genre')
+const BookInstance = require('./models/bookinstance')
 
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.set('strictQuery', false); // Prepare for Mongoose 7
 
-var mongoDB = userArgs[0];
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const mongoDB = userArgs[0];
 
-var authors = []
-var genres = []
-var books = []
-var bookinstances = []
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const authors = []
+const genres = []
+const books = []
+const bookinstances = []
 
 function authorCreate(first_name, family_name, d_birth, d_death, cb) {
   authordetail = {first_name:first_name , family_name: family_name }
   if (d_birth != false) authordetail.date_of_birth = d_birth
   if (d_death != false) authordetail.date_of_death = d_death
   
-  var author = new Author(authordetail);
+  const author = new Author(authordetail);
        
   author.save(function (err) {
     if (err) {
@@ -50,7 +46,7 @@ function authorCreate(first_name, family_name, d_birth, d_death, cb) {
 }
 
 function genreCreate(name, cb) {
-  var genre = new Genre({ name: name });
+  const genre = new Genre({ name: name });
        
   genre.save(function (err) {
     if (err) {
@@ -72,7 +68,7 @@ function bookCreate(title, summary, isbn, author, genre, cb) {
   }
   if (genre != false) bookdetail.genre = genre
     
-  var book = new Book(bookdetail);    
+  const book = new Book(bookdetail);    
   book.save(function (err) {
     if (err) {
       cb(err, null)
@@ -93,7 +89,7 @@ function bookInstanceCreate(book, imprint, due_back, status, cb) {
   if (due_back != false) bookinstancedetail.due_back = due_back
   if (status != false) bookinstancedetail.status = status
     
-  var bookinstance = new BookInstance(bookinstancedetail);    
+  const bookinstance = new BookInstance(bookinstancedetail);    
   bookinstance.save(function (err) {
     if (err) {
       console.log('ERROR CREATING BookInstance: ' + bookinstance);
