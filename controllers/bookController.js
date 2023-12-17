abba9d14f62175c70ca66655650cffc87833ef2c
Hamish Willee <hamishwillee@gmail.com>
2023-12-18 09:43:22 +1100

    Book update - improve the genre selection

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -202,7 +202,7 @@ exports.book_delete_post = asyncHandler(async (req, res, next) => {
 exports.book_update_get = asyncHandler(async (req, res, next) => {
   // Get book, authors and genres for form.
   const [book, allAuthors, allGenres] = await Promise.all([
-    Book.findById(req.params.id).populate("author").populate("genre").exec(),
+    Book.findById(req.params.id).populate("author").exec(),
     Author.find().sort({ family_name: 1 }).exec(),
     Genre.find().sort({ name: 1 }).exec(),
   ]);
@@ -215,13 +215,9 @@ exports.book_update_get = asyncHandler(async (req, res, next) => {
   }
 
   // Mark our selected genres as checked.
-  for (const genre of allGenres) {
-    for (const book_g of book.genre) {
-      if (genre._id.toString() === book_g._id.toString()) {
-        genre.checked = "true";
-      }
-    }
-  }
+  allGenres.forEach((genre) => {
+    if (book.genre.includes(genre._id)) genre.checked = "true";
+  });
 
   res.render("book_form", {
     title: "Update Book",
